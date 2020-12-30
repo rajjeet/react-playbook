8b6d1f0533dca7c7a965480b6b2fad1738431e9b
Rajjeet Phull <rajjeet.phull@gmail.com>
2020-12-29 21:04:44 -0500

    move index html outside of src dir

--- packages/redux-saga/webpack.config.js
+++ packages/redux-saga/webpack.config.js
@@ -20,5 +20,5 @@ module.exports = {
       }
     ]
   },
-  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html') })]
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'index.html') })]
 };
