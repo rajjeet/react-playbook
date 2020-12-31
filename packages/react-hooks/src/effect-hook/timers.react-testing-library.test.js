b2e0a8f13d1178ffc0d970610370dbcb24c6b166
Rajjeet Phull <rajjeet.phull@gmail.com>
2020-12-31 17:15:10 -0500

    add RTL tests for timer useEffect hook

--- packages/react-hooks/src/effect-hook/timers.react-testing-library.test.js
+++ packages/react-hooks/src/effect-hook/timers.react-testing-library.test.js
@@ -1,8 +1,23 @@
-describe('Timers > React Testing Library', function () {
-  it('should have disabled buttons for first 2 seconds', function () {
+import { render, screen, act } from "@testing-library/react";
+import { TimerEffectsHook } from "./timers";
+import React from "react";
 
+describe('Timers > React Testing Library', function () {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  })
+  afterEach(() => {
+    jest.clearAllTimers();
+  })
+  it('should have disabled buttons for first 2 seconds', function () {
+    render(<TimerEffectsHook/>);
+    expect(screen.getByText("1").hasAttribute("disabled")).toBe(true);
   });
   it('should have enabled buttons after 2 seconds', function () {
-
+    render(<TimerEffectsHook/>);
+    act(() => {
+      jest.runAllTimers();
+    });
+    expect(screen.getByText("1").hasAttribute("disabled")).toBe(false);
   });
 });
