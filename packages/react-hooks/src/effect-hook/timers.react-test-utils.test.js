d6f89801b1dd1afa9901ce123b09d7d460d87b08
Rajjeet Phull <rajjeet.phull@gmail.com>
2020-12-31 17:08:39 -0500

    add test for react test utils for timer useEffect

--- packages/react-hooks/src/effect-hook/timers.react-test-utils.test.js
+++ packages/react-hooks/src/effect-hook/timers.react-test-utils.test.js
@@ -1,8 +1,35 @@
-describe('Timers > React Test Utils', function () {
-  it('should have disabled buttons for first 2 seconds', function () {
+import { render, unmountComponentAtNode } from "react-dom";
+import { TimerEffectsHook } from "./timers";
+import React from "react";
+import { act } from "react-dom/test-utils";
 
+describe('Timers > React Test Utils', function () {
+  let container = null;
+  beforeEach(() => {
+    jest.useFakeTimers();
+    container = document.createElement("div");
+  })
+  afterEach(() => {
+    jest.clearAllTimers();
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  })
+  it('should have disabled buttons for first 2 seconds', function () {
+    act(() => {
+      render(<TimerEffectsHook/>, container);
+    })
+    const button = container.querySelector("button[id='1']")
+    expect(button.hasAttribute("disabled")).toBe(true);
   });
   it('should have enabled buttons after 2 seconds', function () {
-
+    act(() => {
+      render(<TimerEffectsHook/>, container);
+    })
+    act(() => {
+      jest.runAllTimers()
+    });
+    const button = container.querySelector("button[id='1']")
+    expect(button.hasAttribute("disabled")).toBe(false);
   });
 });
